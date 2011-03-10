535c054d58385cf64873618985e10637c4b32601
Atul Varma <varmaa@gmail.com>
2011-03-09 17:44:45 -0800

    Added a sidebar and moved the first chunk of text on the main page into a separate 'mission' section.

--- js/apply-page-template.js
+++ js/apply-page-template.js
@@ -1,12 +1,30 @@
 $(window).ready(function() {
+  var header = $("#header");
+  header.detach();
+  if (header.length == 0)
+    header = null;
+
   var contents = $(document.body).contents();
-  var hasReplacementHeader = $("#header").length != 0;
   contents.detach();
   $(document.body).load('/page-template.html', function() {
-    if (hasReplacementHeader)
-      $("#header").remove();
+    if (header)
+      $("#header.default").replaceWith(header);
     else
-      $("#header .title").text(document.title);
+      header = $("#header.default");
+
+    header.find(".title").text(document.title);
     $(this).find("#contents-placeholder").replaceWith(contents);
+
+    // Ugh, we need to "refresh" the hash if there is one, so the
+    // browser tries to jump to named anchors that might be
+    // mentioned in the content we just injected.
+
+    var hash = window.location.hash;
+    if (hash.length > 1) {
+      window.location.hash = "#";
+      window.setTimeout(function() {
+        window.location.hash = hash;
+      }, 100);
+    }
   });
 });
