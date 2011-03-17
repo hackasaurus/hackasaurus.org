19784fd687c2d2474915a6c5c492cf325b6dddf6
Atul Varma <varmaa@gmail.com>
2011-03-17 15:57:12 -0700

    Changed execution flow of pages to support waiting for multiple ajax requests before displaying the page, and added blog section.

--- js/apply-page-template.js
+++ js/apply-page-template.js
@@ -1,3 +1,5 @@
+var pageLoaders = [];
+
 $(window).ready(function() {
   var header = $("#header");
   header.detach();
@@ -6,6 +8,11 @@ $(window).ready(function() {
 
   var contents = $(document.body).contents();
   contents.detach();
+
+  var templateLoaded = jQuery.Deferred();
+
+  pageLoaders.push(templateLoaded);
+
   $(document.body).load('/page-template.html', function() {
     if (header)
       $("#header.default").replaceWith(header);
@@ -13,20 +20,27 @@ $(window).ready(function() {
       header = $("#header.default");
 
     header.find(".title").text(document.title);
-    $(this).find("#contents-placeholder").replaceWith(contents);
-
-    // Ugh, we need to "refresh" the hash if there is one, so the
-    // browser tries to jump to named anchors that might be
-    // mentioned in the content we just injected.
-
-    var hash = window.location.hash;
-    if (hash.length > 1) {
-      window.location.hash = "#";
-      $(document.body).hide();
-      window.setTimeout(function() {
-        $(document.body).show();
-        window.location.hash = hash;
-      }, 100);
-    }
+    $("#container").find("#contents-placeholder").replaceWith(contents);
+    templateLoaded.resolve();
   });
+
+  window.setTimeout(function() {
+    jQuery.when.apply(jQuery, pageLoaders).then(function() {
+      $(window).trigger("beforepagedisplay");
+
+      // Ugh, we need to "refresh" the hash if there is one, so the
+      // browser tries to jump to named anchors that might be
+      // mentioned in the content we just injected.
+
+      var hash = window.location.hash;
+      if (hash.length > 1) {
+        window.location.hash = "#";
+        $(document.body).hide();
+        window.setTimeout(function() {
+          $(document.body).show();
+          window.location.hash = hash;
+        }, 100);
+      }
+    });
+  }, 0);
 });
