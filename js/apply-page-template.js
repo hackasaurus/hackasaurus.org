93a57bb7e4356b1b34493322ae7c9bbc1b6dd550
Atul Varma <varmaa@gmail.com>
2011-03-20 14:02:53 -0700

    refactored page load logic and added a throbber to the blog page.

--- js/apply-page-template.js
+++ js/apply-page-template.js
@@ -1,4 +1,5 @@
-var pageLoaders = [];
+var pageTemplateLoaded = jQuery.Deferred();
+var pageLoaders = [pageTemplateLoaded];
 
 $(window).ready(function() {
   var header = $("#header");
@@ -12,10 +13,6 @@ $(window).ready(function() {
   contents.detach();
   $(document.body).append(templates);
 
-  var templateLoaded = jQuery.Deferred();
-
-  pageLoaders.push(templateLoaded);
-
   $(document.body).load('/page-template.html', function() {
     if (header)
       $("#header.default").replaceWith(header);
@@ -24,26 +21,24 @@ $(window).ready(function() {
 
     header.find(".title").text(document.title);
     $("#container").find("#contents-placeholder").replaceWith(contents);
-    templateLoaded.resolve();
+    pageTemplateLoaded.resolve();
   });
 
-  window.setTimeout(function() {
-    jQuery.when.apply(jQuery, pageLoaders).then(function() {
-      $(window).trigger("beforepagedisplay");
+  jQuery.when.apply(jQuery, pageLoaders).then(function() {
+    $(window).trigger("beforepagedisplay");
 
-      // Ugh, we need to "refresh" the hash if there is one, so the
-      // browser tries to jump to named anchors that might be
-      // mentioned in the content we just injected.
+    // Ugh, we need to "refresh" the hash if there is one, so the
+    // browser tries to jump to named anchors that might be
+    // mentioned in the content we recently injected.
 
-      var hash = window.location.hash;
-      if (hash.length > 1) {
-        window.location.hash = "#";
-        $(document.body).hide();
-        window.setTimeout(function() {
-          $(document.body).show();
-          window.location.hash = hash;
-        }, 100);
-      }
-    });
-  }, 0);
+    var hash = window.location.hash;
+    if (hash.length > 1) {
+      window.location.hash = "#";
+      $(document.body).hide();
+      window.setTimeout(function() {
+        $(document.body).show();
+        window.location.hash = hash;
+      }, 100);
+    }
+  });
 });
