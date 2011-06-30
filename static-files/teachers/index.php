3ead3641dde1f897736eda6c9799d52104e6ba63
Anna Debenham <anna@maban.co.uk>
2011-06-30 15:31:35 +0100

    Adding a teacher page.

---
+++ static-files/teachers/index.php
@@ -0,0 +1,105 @@
+<?php include_once("../include/module/head.php")?>
+
+<?php include_once("../include/module/header.php")?>
+	
+	<section class="inner-page games" role="main">
+
+	<h1>Hackasaurus for Teachers</h1>
+	
+	<div class="content-primary">
+	
+	<p>The Hackasaurus toolkit is great fun to use in lessons as an introduction to coding. We've prepared a lesson plan that you can use to deliver Hackasaurus workshops where you are.</p>
+	
+	<blockquote>
+		<p>A fantastically simple and powerful tool - exactly what the students often want to do in lessons and a great way to motivate them to learn simple bits of HTML without overwhelming them with the dry interface of a blank Notepad document.</p>
+	</blockquote>
+	<cite><a href="http://www.tes.co.uk/teaching-resource/Web-X-Ray-Goggles-by-Hackasaurus-6074624/">Teacher on the TES website</a></cite>
+	
+	<div class="instructions">
+		<h2>How to hack the web</h2>
+		<p>Duration: 45 minutes</p>
+		<p>Make sure you have everything set up beforehand to give the students plenty of time to play with the tools.</p>
+	 
+		<h3>Overview:</h3>
+		<p>In this activity, students learn how to personalize and remix  websites by hacking. Through guided exploration, discussion, and  remixing html, students will begin to understand how they can integrate these tools into a classroom experience.</p>
+		
+		<h3>Objectives:</h3>
+		<ul>
+			<li>Students will learn how to edit HTML through brainstorming, prototyping and iterative design;</li>
+			<li>Students will discover through exploration of familiar websites, that the web is a plaform for a variety of viewpoints.</li>
+			<li>Students will learn the basics of HTML through use of the hacker goggles.</li>
+			<li>Students will learn how to change content on webpages through the use of the remix functionality of the hacker goggles.</li>
+			<li>Students will create a shareable artifact by using the flickr share feature of the hacker goggles.</li>
+		</ul>
+	 
+		<h3>Materials:</h3>
+		<ul>
+			<li>Laptops or PCs for students or teams of students</li>
+			<li>Projector</li>
+			<li>Latest version of Firefox, Safari, or Chrome</li>
+			<li>Hacker Goggles: http://hackasaurus.org/goggles/</li>
+		</ul>
+	
+		<h3>Deliverables:</h3>
+		<p>By the end of this module, students should produce and present to  their peers a hacked website. Additionally, students will have  shareable artifacts of digital (through flickr) and physical (through  the printable hack website) hacks.</p>
+		
+		<h3>Introduction:</h3>
+		<h4>What is hacking? What is the open web? What is web-making? (5-10 mins)</h4>
+		<p>We have an unique definition for hacking. When we say hack we are talking about remixing content to make new things for the web. We mean hacking as tinkering. We are not implying anything malicious or illegal.</p>
+	
+		<h3>Inspecting the web</h3>
+		<h4>How to look under the bonnet of a website and live-edit the contents of any web page. (20 mins)</h4>
+	
+	    <ul>
+	    	<li>Using a projector and a laptop, pull up a website that you know students will recognize- for example google or facebook</li>
+			<li>Demo the goggles.
+				<ul>
+					<li>PUT ON YOUR GOGGLES</li>
+					<li>LOOK at the code</li>
+					<li>FIND THE TAGS that you want to change or replace</li>
+					<li>EDIT THE CONTENT</li>
+					<li>BUST THE HACK</li>
+				</ul>
+			</li>
+			<li>Now tell students to try out the goggles.</li>
+			<li>Let students play around with the tools and walk around and help them. If you notice someone who made a particularly great hack, call it out and have students explain how they made it.</li>
+		</ul>
+	
+		<h3>Hacking the web: How to re-mix the web, build your own websites and collaborate with others. (10 mins)</h3>
+		
+		<ul>
+			<li>Ask students to "tear out" their webpage and stick it in the Web Pad.</li>
+			<li>Explain that web pages are made out of components like lego or pick and mix, and we can put different things on our canvas.</li>
+			<li>Show students the Hackbook and how to copy and paste hacks across.</li>
+			<li>Ethics (copying is bad)/ Creative commons -mention that there are ways to credit</li>
+		</ul>
+	
+		<h3>Conclusion</h3>
+		<p>If you have time, have students present their work. If you are short on time- have each participant go around and say 1 thing that they learned in the lesson.</p>
+		<p>Show the students where they can get more resources about how to implement this in their classrooms:</p>
+		<ul>
+			<li><a href="http://hackasaurus.org/">The Hackasaurus website</a></li>
+			<li><a href="https://wiki.mozilla.org/Drumbeat/Hackasaurus">Our wiki page</a></li>
+		</ul>
+	
+	</div>
+	
+	</div>
+	
+	<div class="content-secondary">
+<h2>Hackasaurus I.T. checklist</h2>
+<ol>
+	<li>Make sure that the jam space has a stable internet connection.</li>
+	<li>Have laptops or PCs available for students or teams of students.</li>
+	<li>Install the most up-to-date browsers on the computers.  Right now the Hackasaurus tools work reliably on the latest versions of  Firefox, Chrome, and Safari, but support for more browsers is on the  way.</li>
+	<li>Provide a projector that connects to a computer so that students can work publicly.</li>
+	<li>Test all tools to be used in the jams before-hand.</li>	
+</ol>
+
+<?php include_once("../include/module/backpack.php")?>
+
+	</div>
+		
+	</section>
+
+<?php include_once("../include/module/footer.php")?>
