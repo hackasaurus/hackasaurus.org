08c522d581fee5ae977c47a0321f21691384d8c8
Atul Varma <varmaa@gmail.com>
2011-07-31 15:49:04 -0400

    now setup.py uses find_packages().

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 from bootstrap.bootstrap import install_requires
 
 setup(
     name='hackasaurus',
     version='0.1',
-    packages=['hackasaurus'],
+    packages=find_packages(),
     author='The Hackasaurus Team',
     author_email='rawr@hksr.us',
     install_requires=install_requires
