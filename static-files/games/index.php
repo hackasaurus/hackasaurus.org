cff46664b414c80938772a8fabc927c53a2c62e0
Atul Varma <varmaa@gmail.com>
2011-08-03 19:33:19 -0400

    replaced link to hackasaurus mission with a link to the parable.

--- static-files/games/index.php
+++ static-files/games/index.php
@@ -8,21 +8,20 @@
 	
 	<div class="content-primary">
 
+				<img src="http://hackasaurus.org/include/image/photo-hackers-03.jpg"
+    alt="photo-hackers-03" /> <h2>Coming Soon</h2>
 		
-		<img src="http://hackasaurus.org/include/image/photo-hackers-03.jpg" alt="photo-hackers-03" />
-		<h2>Coming Soon</h2>
+		 <p>Hackasaurus games make it fun and easy to learn about the basic
+    building blocks of the web. They're still in development. But in the mean
+    time, <a href="http://toolness.github.com/hackasaurus-parable/">here's a
+    sneak peek at our first Hackasaurus mission</a>.</p>
 		
-		<p>Hackasaurus games make it fun and easy to learn about the basic building blocks of the web. They're still in development. But in the mean time, <a href="https://secure.toolness.com/webxray/missions/one/">here's a sneak peek at our first Hackasaurus mission</a>.</p>
+		 </div>
 		
+		 <div class="content-secondary"> <?php
+    include_once("../include/module/backpack.php") ?> <?php
+    include_once("../include/module/events.php") ?> </div>
 		
-			
-	</div>
-	
-	<div class="content-secondary">
-		<?php include_once("../include/module/backpack.php") ?>
-		<?php include_once("../include/module/events.php") ?>
-	</div>
-		
-	</section>
+		 </section>
 
 <?php include_once("../include/module/footer.php") ?>
