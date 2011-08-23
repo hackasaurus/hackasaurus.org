0f72dce3cb36000d7bf9e398167b29e30a4ab732
Atul Varma <varmaa@gmail.com>
2011-08-23 14:06:13 -0700

    Removed support for PHP, since the site no longer uses any.

--- manage.py
+++ manage.py
@@ -13,7 +13,7 @@ sys.path.insert(0, path('vendor'))
 import hackasaurus_dot_org
 import jinja2
 from hackasaurus.management import execute_manager
-from hackasaurus.lamp_emulation import apply_htaccess, load_php
+from hackasaurus.lamp_emulation import apply_htaccess
 
 static_files_dir = path('static-files')
 
@@ -22,9 +22,6 @@ def handle_html_file_as_jinja2_template(root_dir, fullpath):
     env = jinja2.Environment(loader=loader)
     template = env.get_template(fullpath[len(root_dir):])
     return ('text/html', template.render().encode('utf-8'))
-
-def handle_php_file(root_dir, fullpath):
-    return ('text/html', load_php(root_dir, fullpath))
 
 def wsgi_api_handler(env, start):
     if env['PATH_INFO'].startswith('/wsgi/'):
@@ -41,8 +38,6 @@ if __name__ == '__main__':
         static_files_dir=static_files_dir,
         ext_handlers={
             '.html': handle_html_file_as_jinja2_template,
-            '.php': handle_php_file
             },
         handlers=[wsgi_api_handler, htaccess_handler],
-        default_filenames=['index.html', 'index.php'],
         )
