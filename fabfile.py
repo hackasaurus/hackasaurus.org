accc2e8c3fc11e56c4eda8756a5f3bba476b4f1b
Atul Varma <varmaa@gmail.com>
2011-11-18 12:30:58 -0800

    Add explicit lcd

--- fabfile.py
+++ fabfile.py
@@ -3,8 +3,11 @@ import os
 from fabric.api import *
 from fabric.contrib.project import rsync_project
 
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
 @task
 def deploy():
-    local('python manage.py build')
-    rsync_project(remote_dir='/var/hackasaurus.org',
-                  local_dir='dist/')
+    with lcd(ROOT):
+        local('python manage.py build')
+        rsync_project(remote_dir='/var/hackasaurus.org',
+                      local_dir='dist/')
