b9e17373a0088b0efa5bd415cd72049ce018e4f1
Atul Varma <varmaa@gmail.com>
2012-01-24 16:57:55 -0800

    fixed bug in fabfile

--- fabfile.py
+++ fabfile.py
@@ -5,10 +5,14 @@ from fabric.contrib.project import rsync_project
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
 
+def lexists(path):
+    with settings(warn_only=True):
+        return local('test -e %s' % path).succeeded
+
 @task
 def deploy():
     with lcd(ROOT):
-        if not os.path.exists('locale'):
+        if not lexists('locale'):
             local('git clone https://github.com/hackasaurus/hackasaurus-locales.git locale')
         local('python manage.py compilemessages')
         local('python manage.py build')
