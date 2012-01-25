1a36e07885fde7b62286ba268136718714b94279
Atul Varma <varmaa@gmail.com>
2012-01-24 16:16:16 -0800

    for fab deploy, clone hackasaurus-locales if needed and run manage.py compilemessages before building.

--- fabfile.py
+++ fabfile.py
@@ -8,6 +8,9 @@ ROOT = os.path.abspath(os.path.dirname(__file__))
 @task
 def deploy():
     with lcd(ROOT):
+        if not os.path.exists('locale'):
+            local('git clone https://github.com/hackasaurus/hackasaurus-locales.git locale')
+        local('python manage.py compilemessages')
         local('python manage.py build')
         rsync_project(remote_dir='/var/hackasaurus.org',
                       local_dir='dist/')
